326948fb287e594f1c694dc318e125bd2eddcd1a
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2024-07-26 20:53:32 +0800

    login page

--- src/app/login/component.tsx
+++ src/app/login/component.tsx
@@ -7,7 +7,7 @@ export default function LoginComponent() {
       <aside className="relative h-full flex items-center justify-center lg:gap-y-8 space-x-4 lg:space-x-0 lg:flex-col py-4 lg:p-0">
         <div className="z-0 w-[200px] h-full absolute -right-[100px] top-0 bg-black hidden lg:block">
           <div className="relative w-full h-full grid grid-rows-2">
-            <div className="bg-[#0D41F8]">
+            <div className="bg-[#0D41F8] z-0">
               <div className="absolute left-0 top-0 w-1/2 h-full rounded-br-full bg-[#0D41F8]" />
             </div>
             <div className="bg-[#F6FFF1]">
@@ -15,14 +15,16 @@ export default function LoginComponent() {
             </div>
           </div>
         </div>
-        <div className="hidden lg:block w-[400px] h-[400px] xl:w-[600px] xl:h-[600px]">
-          <Image className="z-10 w-full h-full" src="/buena-logo.svg" alt="Municipal Logo" width={597} height={615} priority />
+        <div className="z-10 hidden lg:block w-[400px] h-[400px] xl:w-[600px] xl:h-[600px]">
+          <Image className=" w-full h-full" src="/buena-logo.svg" alt="Municipal Logo" width={597} height={615} priority />
         </div>
         <h1 className="z-10 hidden lg:block uppercase font-[700] px-4 leading-[40.23px] text-center text-white w-fit text-[33px]">
           MTAP MANAGEMENT SYSTEM
         </h1>
         <div className="grid grid-cols-3 gap-x-4 lg:hidden">
-          <Image className="z-10 w-[120px] h-[120px]" src="/buena-logo.svg" alt="Municipal Logo" width={597} height={615} priority />
+          <div className="z-10 w-[120px] h-[120px]">
+            <Image className="w-full h-full" src="/buena-logo.svg" alt="Municipal Logo" width={597} height={615} priority />
+          </div>
           <h1 className="z-10 uppercase font-[700] leading-[40.23px] text-left text-white text-[28px] w-[100px]">
             MTAP MANAGEMENT SYSTEM
           </h1>
