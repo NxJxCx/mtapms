e8c85b91000d85a28eb7da723fe48da70cf6207a
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2024-08-11 20:51:57 +0800

    okay

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,4 +1,9 @@
+import mongodbConnect from '@app/lib/db';
+import Admin from '@app/models/Admin';
+import Student from '@app/models/Student';
+import { Roles } from '@app/types';
 import { type JWTPayload, SignJWT, jwtVerify } from 'jose';
+import { cookies } from 'next/headers';
 import 'server-only';
 
 const secretKey = process.env.SESSION_SECRET
@@ -12,11 +17,81 @@ export async function encrypt(payload: { [key: string]: any }, hours: number = 8
     .sign(encodedKey)
 }
 
-export async function decrypt(session?: string): Promise<JWTPayload | { [key: string]: any } | void> {
+export async function decrypt(session?: string): Promise<JWTPayload | { [key: string]: any } | null> {
   try {
     const { payload } = await jwtVerify(session || '', encodedKey, {
       algorithms: ['HS256'],
     })
     return payload
   } catch (error) {}
+  return null;
+}
+
+export async function generateSessionPayload(account: Roles, userId: string, expHours: number = 8) {
+  await mongodbConnect();
+  try {
+    const AccountModel = account === Roles.Admin ? Admin : Student;
+    const user = await AccountModel.findOne({ _id: userId }).select('-password').lean().exec()
+    if (user) {
+      return {
+        user,
+        expiresAt: new Date(Date.now() + expHours * 60 * 60 * 1000)
+      }
+    }
+  } catch (e) {}
+  return null;
+}
+
+export async function createSession(role: Roles, userId: string, expHours: number = 8): Promise<boolean> {
+  const payload = await generateSessionPayload(role, userId, expHours);
+  if (!payload) {
+    return false;
+  }
+
+  const session = await encrypt(payload, expHours)
+
+  cookies().set('auth', session, {
+    httpOnly: true,
+    secure: true,
+    expires: payload.expiresAt as Date,
+    sameSite: 'lax',
+    path: '/',
+  })
+
+  return true;
+}
+
+export async function getSession(): Promise<JWTPayload | { [key: string]: any } | null> {
+  const cookie = cookies().get('auth')
+  if (cookie && cookie.value) {
+    const session = await decrypt(cookie.value)
+    if (session && (Math.floor(Date.now() / 1000) > (session as any).exp)) {
+      await destroySession()
+      return null;
+    }
+    return session
+  }
+  return null;
+}
+
+export async function destroySession() {
+  cookies().delete('auth')
+  const expires = new Date()
+  expires.setFullYear(1901, 1, 1)
+  cookies().set('auth', '', {
+    httpOnly: true,
+    secure: true,
+    expires,
+    sameSite: 'lax',
+    path: '/',
+  })
+}
+
+export async function updateSession(role: Roles): Promise<boolean> {
+  const session = await getSession();
+  if (session) {
+    const result = await createSession(role, session.user.userId)
+    return result;
+  }
+  return false;
 }
