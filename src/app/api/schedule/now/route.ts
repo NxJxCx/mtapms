14452f22fe99e7e0c075f181d50ae9a9c460c87b
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2024-09-12 00:21:50 +0800

    bug fixw

--- src/app/api/schedule/now/route.ts
+++ src/app/api/schedule/now/route.ts
@@ -13,18 +13,28 @@ export async function GET(request: NextRequest) {
     const session = await getSession()
     if (session?.user?.role === Roles.Applicant) {
       const action = request.nextUrl.searchParams.get('action')
-      const result = await Schedule.find({}).select('academicYear range orientationDate examDate interviewDate' + (action === 'scheduleandresult' ? ' examScores' : '')).exec()
-      const dataRecent: ScheduleModel = result.length === 0 ? null : JSON.parse(JSON.stringify(result)).reduce((init: ScheduleModel, item: ScheduleModel) => !init ? item : item.academicYear > init.academicYear ? item : init, null)
-      const data = !!dataRecent && (new Date(dataRecent.range.startDate)).getTime() <= (new Date()).getTime() && (new Date(dataRecent.range.endDate)).getTime() >= (new Date()).getTime() ? dataRecent : null
-      if (!!data) {
-        const student = await Student.findById(session.user._id).exec()
-        if ((action === 'documents' || action === 'scheduleandresult') && !!student && student?.applicationForm?.scheduleId.toString() === data._id!.toString()) {
-          return NextResponse.json({ data })
-        }
-        if (!student?.applicationForm || student?.applicationForm?.scheduleId.toString() !== data._id as string) {
-          return NextResponse.json({ data })
-        } else if (!!student && student?.applicationForm?.scheduleId.toString() === data._id!.toString()) {
-          return NextResponse.json({ data: true })
+      const selection = 'academicYear range orientationDate examDate interviewDate' + (action === 'scheduleandresult' ? ' examScores' : '')
+      const result = await Schedule.find({}).select(selection).lean<ScheduleModel[]>().exec()
+      result.sort((a, b) => b.academicYear - a.academicYear)
+      const dataRecent: ScheduleModel = result?.[0] || null
+      if (!!dataRecent) {
+        const recentStartDate = new Date(dataRecent?.range.startDate)
+        recentStartDate.setHours(0, 0, 0, 0)
+        const recentEndDate = new Date(dataRecent?.range.endDate)
+        recentEndDate.setHours(0, 0, 0, 0)
+        const now = new Date()
+        now.setHours(0, 0, 0, 0)
+        const data = !!dataRecent && recentStartDate.getTime() <= now.getTime() && recentEndDate.getTime() >= now.getTime() ? dataRecent : null
+        if (!!data) {
+          const student = await Student.findById(session.user._id).exec()
+          console.log(student, "st", data)
+          if ((action === 'documents' || action === 'scheduleandresult') && !!student && student?.applicationForm?.scheduleId.toString() === data._id!.toString()) {
+            return NextResponse.json({ data })
+          } else  if (!student?.applicationForm && student?.applicationForm?.scheduleId.toString() !== data._id as string) {
+            return NextResponse.json({ data })
+          } else if (!!student && student?.applicationForm?.scheduleId.toString() === data._id!.toString()) {
+            return NextResponse.json({ data: true })
+          }
         }
       }
     } else if (session?.user?.role === Roles.Admin) {
