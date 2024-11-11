d1756e0edad483a1f49408921aee3bfcfec8c741
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2024-11-11 09:24:55 +0800

    ok barchart

--- src/app/(main)/admin/dashboard/component.tsx
+++ src/app/(main)/admin/dashboard/component.tsx
@@ -5,19 +5,50 @@ import { UsersIcon } from "@heroicons/react/16/solid";
 import clsx from "clsx";
 import { Montserrat, Outfit } from "next/font/google";
 import { useEffect, useState } from "react";
+import ReactApexChart from "react-apexcharts";
+
 const outfit = Outfit({ subsets: ["latin"], weight: ["400", "600", "700"],  })
 
 const montserrat = Montserrat({ subsets: ["latin"], weight: ["400", "600", "700"] })
 
+
 function Charts(props: any) {
+  const [chartSeries, setChartSeries] = useState([{
+    data: props.data || []
+  }]);
+  const [chartOptions, setChartOptions] = useState<ApexCharts.ApexOptions>({
+    chart: {
+      type: 'bar',
+      height: 350,
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 4,
+        borderRadiusApplication: 'end',
+        horizontal: true,
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    xaxis: {
+      categories: props.categories || [],
+    }
+  });
+  useEffect(() => {
+    setChartOptions({...chartOptions, xaxis: { categories: props.categories || [] }});
+    setChartSeries([{ data: props.data || []}]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props]);
+
   return (
     <div className="flex flex-wrap mb-4 xl:gap-x-4 gap-y-4 xl:space-y-0">
       <div className="flex-grow rounded-lg bg-white border p-4">
         <div className="flex justify-between gap-x-4">
           <h2 className="font-[500] text-[20px] leading-[30px]">
-            Scholar Analytics
+            Scholarship Grantees
           </h2>
-          <div className="border rounded bg-white px-3 py-1">
+          {/* <div className="border rounded bg-white px-3 py-1">
             <select title="Analytics Data By" className="font-[400] text-[12px] leading-[18px] bg-white">
               <option value="monthly">
                 Monthly
@@ -26,10 +57,15 @@ function Charts(props: any) {
                 Yearly
               </option>
             </select>
-          </div>
+          </div> */}
         </div>
         <div className="min-w-[700px] min-h-[250px]">
-          
+          <ReactApexChart
+            options={chartOptions}
+            series={chartSeries}
+            type="bar"
+            height={350}
+          />
         </div>
       </div>
       {/* <div className="rounded-lg p-4 border flex flex-col flex-grow max-w-[400px] min-w-[300px] min-h-[300px] bg-white">
@@ -74,6 +110,15 @@ function Charts(props: any) {
     </div>
   )
 }
+function displayNominalYear(yr: number|string) {
+  return yr.toString().endsWith("1")
+  ? yr + 'st Year'
+  : yr.toString().endsWith("2")
+  ? yr + 'nd Year'
+  : yr.toString().endsWith("3")
+  ? yr + 'rd Year'
+  : yr + 'th Year';
+}
 
 export default function AdminDashboardComponent() {
   const [statisticNumbers, setStatisticNumbers] = useState<{title: string, value: number, icon: JSX.Element }[]>([
@@ -98,33 +143,49 @@ export default function AdminDashboardComponent() {
       icon: <UsersIcon className="h-[20px] w-[20px]" />
     },
   ])
+  const [barChartData, setBarChartData] = useState<number[]>([]);
+  const [barChartCategories, setBarChartCategories] = useState<string[]>([]);
 
   const fetchDataDashboard = () => {
     const url = new URL('/api/admin/dashboard', window.location.origin)
     fetch(url)
       .then(resp => resp.json())
-      .then(({ data }) => setStatisticNumbers([
-        {
-          title: "Total Scholar",
-          value: data.totalScholars || 0,
-          icon: <UsersIcon className="h-[20px] w-[20px]" />
-        },
-        {
-          title: "Total Graduates",
-          value: data.totalGraduates || 0,
-          icon: <UsersIcon className="h-[20px] w-[20px]" />
-        },
-        {
-          title: "Current No. of Applicants",
-          value: data.totalApplicants,
-          icon: <UsersIcon className="h-[20px] w-[20px]" />
-        },
-        {
-          title: "Total No. of Users",
-          value: data.totalUsers || 0,
-          icon: <UsersIcon className="h-[20px] w-[20px]" />
-        },
-      ]))
+      .then(({ data }) => {
+        setStatisticNumbers([
+          {
+            title: "Total Scholar",
+            value: data.totalScholars || 0,
+            icon: <UsersIcon className="h-[20px] w-[20px]" />
+          },
+          {
+            title: "Total Graduates",
+            value: data.totalGraduates || 0,
+            icon: <UsersIcon className="h-[20px] w-[20px]" />
+          },
+          {
+            title: "Current No. of Applicants",
+            value: data.totalApplicants,
+            icon: <UsersIcon className="h-[20px] w-[20px]" />
+          },
+          {
+            title: "Total No. of Users",
+            value: data.totalUsers || 0,
+            icon: <UsersIcon className="h-[20px] w-[20px]" />
+          },
+        ]);
+        if (data.listOfGranteesPerYearLevel?.length > 0) {
+          let barData: number[] = [];
+          let barCat: string[] = [];
+          [1,2,3,4].forEach((yr: number) => {
+            barCat.push(displayNominalYear(yr));
+            barData.push(data.listOfGranteesPerYearLevel.find((v: any) => v.yearLevel.toString() === yr.toString())?.grantees || 0);
+          });
+          console.log(data.listOfGranteesPerYearLevel);
+          console.log(barCat, barData);
+          setBarChartCategories(barCat);
+          setBarChartData(barData);
+        }
+      })
   }
 
   useEffect(() => {
@@ -134,7 +195,7 @@ export default function AdminDashboardComponent() {
   return (
     <div className={clsx("p-4", outfit.className)}>
       <StatisticNumbers items={statisticNumbers} className={clsx("mb-3", outfit.className)} />
-      <Charts />
+      <Charts data={barChartData} categories={barChartCategories} />
     </div>
   )
 }
--- src/app/api/admin/dashboard/route.ts
+++ src/app/api/admin/dashboard/route.ts
@@ -7,6 +7,7 @@ import Student from "@app/models/Student";
 import { Roles, ScheduleModel, StudentModel } from "@app/types";
 import moment from "moment-timezone";
 import { NextRequest, NextResponse } from "next/server";
+import { YearLevel } from '../../../../types/index';
 
 export async function GET(request: NextRequest) {
   await mongodbConnect()
@@ -19,7 +20,18 @@ export async function GET(request: NextRequest) {
       const latestAcademicYear = latestSchedule.length > 0 ? latestSchedule[0].academicYear : (moment.tz('Asia/Manila').toDate()).getFullYear()
       const graduates = scholars.filter(s => latestAcademicYear - (s.applicationForm!.scheduleId as ScheduleModel).academicYear > 4 - s.applicationForm!.yearLevel)
       const totalApplicants = await Student.find({ applicationForm: { $exists: true }, 'applicationForm.scheduleId': latestSchedule?.[0]?._id?.toString() }).countDocuments().exec()
-      const data = { totalScholars: scholars.length, totalGraduates: graduates.length, totalUsers, totalApplicants }
+      const listOfGranteesPerYearLevel = scholars.reduce((initial: ({ yearLevel: YearLevel, grantees: number })[], scholar: StudentModel) => {
+        const yearLevel: YearLevel = scholar.applicationForm!.yearLevel;
+        const found = initial.findIndex((s: { yearLevel: YearLevel, grantees: number }) => s.yearLevel === yearLevel)
+        if (found === -1) {
+          initial.push({ yearLevel, grantees: 1 })
+        } else {
+          initial[found].grantees += 1;
+        }
+        return [...initial];
+      }, []);
+      console.log(listOfGranteesPerYearLevel)
+      const data = { totalScholars: scholars.length, totalGraduates: graduates.length, totalUsers, totalApplicants, listOfGranteesPerYearLevel }
       return NextResponse.json({ data })
     }
   } catch (e) {
